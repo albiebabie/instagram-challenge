92bf0d8cbaa20a69af09df4dfe3e246778631436
albiebabie <albiebabie@gmail.com>
2016-08-28 18:30:40 +0100

    tests passing - got object array displaying for some reason

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,23 @@
 class PhotosController < ApplicationController
 
   def index
-    @photo = Photo.all
+    @photos = Photo.all
+  end
+
+  def new
+    @photos = Photo.new
+  end
+
+  def create
+    @photos = Photo.new(photo_params)
+    if @photos.save
+      redirect_to photos_path
+    else
+      render 'new'
+    end
+  end
+
+  def photo_params
+    params.require(:photo).permit(:caption, :image)
   end
 end
--- spec/features/photos_spec.rb
+++ spec/features/photos_spec.rb
@@ -18,13 +18,27 @@ feature 'Photos' do
 
     scenario 'should display the added photos' do
       visit '/photos'
+      expect(page).not_to have_content('No Photos yet')
       expect(page).to have_content('Test')
       expect(page).to have_content('Image')
     end
   end
-  #
-  # context 'have been added' do
-  #   scenario 'should display the added photos' do
+
+  context 'can be added' do
+    scenario 'prompts user to add a photo, then displays the new photo' do
+      visit '/photos'
+      click_link 'Add Photo'
+      attach_file 'Image', '/Users/Albie/Desktop/test_image.jpg'
+      fill_in 'Caption', with: 'Cheeky'
+      click_button 'Create Photo'
+      expect(current_path).to eq('/photos')
+      expect(page).to have_content('Cheeky')
+      expect(page).to have_css("img[src*='test_image.jpg']")
+    end
+  end
+
+  # context 'can be added' do
+  #   scenario 'prompts user to add a photo, then displays the new photo' do
   #     visit '/photos'
   #     click_link 'Add Photo'
   #     attach_file 'Image', '/Users/Albie/Desktop/test_image.jpg'
