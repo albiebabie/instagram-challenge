b588f9ffaebfc22426b93fcea7b437907529398c
albiebabie <albiebabie@gmail.com>
2016-08-28 13:04:05 +0100

    should display the added photos

--- spec/features/photos_spec.rb
+++ spec/features/photos_spec.rb
@@ -11,20 +11,33 @@ feature 'Photos' do
   end
 
   context 'have been added' do
+    before do
+      Photo.create(caption: 'Test')
+      Photo.create(caption: 'Image')
+    end
+
     scenario 'should display the added photos' do
       visit '/photos'
-      click_link 'Add Photo'
-      attach_file 'Image', '/Users/Albie/Desktop/test_image.jpg'
-      fill_in 'Caption', with: 'Cheeky'
-      click_button 'Create Photo'
-      click_link 'Add Photo'
-      attach_file 'Image', '/Users/Albie/Desktop/test_2.jpg'
-      fill_in 'Caption', with: 'Wave'
-      click_button 'Create Photo'
-      expect(page).to have_content('Cheeky')
-      expect(page).to have_css("img[src*='test_image.jpg']")
-      expect(page).to have_content('Wave')
-      expect(page).to have_css("img[src*='test_2.jpg']")
+      expect(page).to have_content('Test')
+      expect(page).to have_content('Image')
     end
   end
+  #
+  # context 'have been added' do
+  #   scenario 'should display the added photos' do
+  #     visit '/photos'
+  #     click_link 'Add Photo'
+  #     attach_file 'Image', '/Users/Albie/Desktop/test_image.jpg'
+  #     fill_in 'Caption', with: 'Cheeky'
+  #     click_button 'Create Photo'
+  #     click_link 'Add Photo'
+  #     attach_file 'Image', '/Users/Albie/Desktop/test_2.jpg'
+  #     fill_in 'Caption', with: 'Wave'
+  #     click_button 'Create Photo'
+  #     expect(page).to have_content('Cheeky')
+  #     expect(page).to have_css("img[src*='test_image.jpg']")
+  #     expect(page).to have_content('Wave')
+  #     expect(page).to have_css("img[src*='test_2.jpg']")
+  #   end
+  # end
 end
