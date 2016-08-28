074b17276100aa2e476a6195a6decb6592929af0
albiebabie <albiebabie@gmail.com>
2016-08-28 11:53:30 +0100

    photos can be added - unable to find field image

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
 
   def index
-    # raise "Hello from the index action"
+    
   end
 end
--- spec/features/photos_spec.rb
+++ spec/features/photos_spec.rb
@@ -1,11 +1,30 @@
 require 'rails_helper'
 
-feature 'photos' do
-  context 'no photos have been added' do
-    scenario 'should display a prompt to add a photo' do
+feature 'Photos' do
+
+  context 'have not been added' do
+    scenario 'should display a prompt to add a Photo' do
       visit '/photos'
-      expect(page).to have_content 'No photos yet'
-      expect(page).to have_link 'Add photo'
+      expect(page).to have_content 'No Photos yet'
+      expect(page).to have_link 'Add Photo'
+    end
+  end
+
+  context 'have been added' do
+    scenario 'should display the added photos' do
+      visit '/photos'
+      click_link 'Add Photo'
+      attach_file 'Image', '/Users/Albie/Desktop/test_image.jpg'
+      fill_in 'Caption', with: 'Cheeky'
+      click_button 'Create Photo'
+      click_link 'Add Photo'
+      attach_file 'Image', '/Users/Albie/Desktop/test_2.jpg'
+      fill_in 'Caption', with: 'Wave'
+      click_button 'Create Photo'
+      expect(page).to have_content('Cheeky')
+      expect(page).to have_css("img[src*='test_image.jpg']")
+      expect(page).to have_content('Wave')
+      expect(page).to have_css("img[src*='test_2.jpg']")
     end
   end
 end
